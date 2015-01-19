a7e7170a7d9e71b203e42eaf03d1c89b836cdffb
Brandon P <brandonp@nau.edu>
2015-01-19 05:30:26 -0700

    Bootstrap actually works now. Added basic template stuff.

--- theBand/theBand/settings.py
+++ theBand/theBand/settings.py
@@ -24,6 +24,8 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'theBand/templates')]
+
 ALLOWED_HOSTS = []
 
 
@@ -81,3 +83,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+)
