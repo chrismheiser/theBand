84b8638ff3623c0b898505e05f94418b34c036d2
Brandon P <brandonp@nau.edu>
2015-01-19 22:55:19 -0700

    Added routing for pretty urls.

--- theBand/theBand/urls.py
+++ theBand/theBand/urls.py
@@ -2,11 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'theBand.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'theBand.views.home', name='home'),
+    url(r'^about/', views.about),
+    url(r'^discog/', views.discog),
+    url(r'^tourdates/', views.tourdates),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- theBand/theBand/views.py
+++ theBand/theBand/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 def home(request):
 	return render(request, 'index.html', {})
 
+def about(request):
+	return render(request, 'about.html', {})
+
+def discog(request):
+	return render(request, 'discog.html', {})
+
+def tourdates(request):
+	return render(request, 'tourdates.html', {})
